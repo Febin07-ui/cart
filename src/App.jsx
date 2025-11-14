cd338ef01048506e77fe4bba8aad1e4e513cfe9c
Febin07-ui <febinkulangara23@gmail.com>
2025-11-14 13:02:42 +0530

    pages created

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+import { Route, Routes } from "react-router-dom"
+import Home from "./assets/pages/Home"
+import Wishist from "./assets/pages/Wishist"
+import Cart from "./assets/pages/Cart"
+import View from "./assets/pages/View"
+import Pnf from "./assets/pages/Pnf"
+import Footer from "./assets/components/Footer"
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/wishlist' element={<Wishist/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/products/:id/view' element={<View/>}/>
+        <Route path='/*' element={<Pnf/>}/>
+      </Routes>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/pages/Wishist.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Wishist() {
+  return (
+    <div>Wishist</div>
+  )
+}
+
+export default Wishist
